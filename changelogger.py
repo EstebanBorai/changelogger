29f4c3f01e7ee026cbb2ac13259549d9c5913a28
Esteban Borai <estebanborai@outlook.com>
2019-06-02 22:31:07 -0300

    Add capability to create CHANGELOG

--- changelogger.py
+++ changelogger.py
@@ -1,5 +1,8 @@
-import subprocess
 import re
+import os
+import subprocess
+
+current_directory = os.getcwd()
 
 leading_4_spaces = re.compile('^    ')
 
@@ -40,4 +43,18 @@ def get_commits():
         save_current_commit()
     return commits
 
-get_commits()
+def create_changelog():
+  changelog_dir = current_directory + '/CHANGELOG'
+  try:
+    with open(changelog_dir, 'r') as current_changelog:
+      print(current_changelog)
+  except IOError:
+    print('There\'s no CHANGELOG in this project.')
+    print('Creating a new CHANGELOG')
+    with open(changelog_dir, 'w') as new_changelog:
+      new_changelog.write('# Changelog\n')
+      new_changelog.write('All notable changes to this project will be documented in this file.\n\n')
+      new_changelog.write('The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),\n')
+      new_changelog.write('and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).\n\n')
+
+create_changelog()
