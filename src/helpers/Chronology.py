24e270c284f2b0357540500a0f7154db0ba08cca
Esteban Borai <estebanborai@outlook.com>
2019-06-06 00:56:41 -0300

    Add capability to get commits

--- src/helpers/Chronology.py
+++ src/helpers/Chronology.py
@@ -1,9 +1,15 @@
+import re
+
 class Chronology:
   commits = None
   commit_days = {}
 
   def __init__(self, commits):
     self.commits = commits
+
+  def get_date(self, commit_date):
+    date_str = re.findall(r'[^Date:]([A-Za-z]{3})[\s]([0-9]{1,2})[\s](?:[\d]{2}[\:][\d]{2}[\:][\d]{2}[\s])([0-9]{4})', commit_date)
+    return date_str
 
   def get_by_date(self):
     if self.commits == None:
---
+++ src/helpers/get_commits.py
@@ -0,0 +1,44 @@
+import subprocess
+import sys
+import re
+
+def __git_log():
+  bash_response = subprocess.check_output(
+    ['git', 'log'], stderr=subprocess.STDOUT
+  ).decode(sys.stdout.encoding)
+  
+  return str(bash_response).split('\n')
+
+def __normalize(log_output):
+  current = {}
+  commits = []
+
+  commit_id = lambda l : l.split('commit ')[1]
+
+  for line in log_output:
+    if line.startswith('commit'):
+      if len(list(current.keys())) is 0:
+        current['commit'] = commit_id(line)
+      else:
+        commits.append(current)
+        current = {}
+        current['commit'] = commit_id(line)
+    else:
+      try:
+        key, value = line.split(':', 1)
+        current[key.lower()] = value.strip()
+      except ValueError:
+        if line is '':
+          pass
+        else:
+          commit_message = re.compile('^    ').sub('', line)
+          current['message'] = commit_message
+  
+  return commits
+  
+def get_commits():
+  commits = __git_log()
+  return __normalize(commits)
+
+
+print(get_commits())
