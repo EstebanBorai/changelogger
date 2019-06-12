a5efc2225eacb6b9208e396f68b97f6f5c0b1a7c
Esteban Borai <estebanborai@outlook.com>
2019-06-11 23:25:34 -0300

    Enhance Tokenization

--- src/helpers/tokenize.py
+++ src/helpers/tokenize.py
@@ -3,27 +3,44 @@ from helpers.chronologize import chronologize
 
 class Tokenize:
   tokens = None
+  token_list = None
   commits = None
 
   def __init__(self, commits):
     self.tokens = read_config()['tokens']
     self.commits = commits
 
-  def __resolve_token(self, date_commits):
-    aggregate = {}
+    all_tokens = []
 
-    for commit in date_commits:
-      message = commit['message']
-      for token_set in self.tokens.items():
-        for tkn in token_set[1]:
-          if tkn.lower() in message.lower():
-            if tkn in aggregate:
-              aggregate[tkn].append(commit)
-            else:
-              token_list = list()
-              aggregate[tkn] = token_list
-              aggregate[tkn].append(commit)
+    for _, token_items in self.tokens.items():
+      for tkn in token_items:
+        all_tokens.append(tkn)
     
+    self.token_list = all_tokens
+
+  def __get_token_parent(self, token):
+    for token_parent in self.tokens.keys():    
+      if token in self.tokens[token_parent]:
+        return token_parent
+
+  def __resolve_token(self, date_commits):
+    aggregate = {
+      'Misc': []
+    }
+    
+    for commit in date_commits:
+      for token in self.token_list:
+        if token.lower() in commit['message'].lower().split():
+          token_parent = self.__get_token_parent(token)
+          if token_parent in aggregate:
+            aggregate[token_parent].append(commit)
+          else:
+            token_list = list()
+            aggregate[token_parent] = token_list
+            aggregate[token_parent].append(commit)
+        else:
+          aggregate['Misc'].append(commit)
+
     return aggregate
 
   def tokenize(self):
